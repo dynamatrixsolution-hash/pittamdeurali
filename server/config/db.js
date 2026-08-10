956a375c5b75d582fc9830a89d7d30ae4da8157d
abhishekchaudhary12377 <135638913+abhishekchaudhary12377@users.noreply.github.com>
2026-08-10 11:34:19 +0545

    Support MONGO_URI in db connection config

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    let mongoUrl = process.env.MONGODB_URL;
+    let mongoUrl = process.env.MONGODB_URL || process.env.MONGO_URI;
     if (!mongoUrl) {
-      console.warn('MONGODB_URL env variable not found. Falling back to local MongoDB (mongodb://127.0.0.1:27017/pittamdeurali).');
+      console.warn('MONGODB_URL or MONGO_URI env variable not found. Falling back to local MongoDB (mongodb://127.0.0.1:27017/pittamdeurali).');
       mongoUrl = 'mongodb://127.0.0.1:27017/pittamdeurali';
     }
     const conn = await mongoose.connect(mongoUrl);
